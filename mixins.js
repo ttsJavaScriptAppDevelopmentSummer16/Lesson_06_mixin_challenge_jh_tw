b6bc0d76b08fa2d5ec74dc621f339f2f2709cc47
Jude Harding <jharding@genesisgroupusa.com>
2016-08-08 22:30:46 -0400

    modifications

--- mixins.js
+++ mixins.js
@@ -1,17 +1,36 @@
 
-User.prototype = {
+// Create a user profile object that contains properties for
+// name
+// address
+// city
+// state
+// zipcode
+// avatar
+
+var userProfile = {
 	name: '',
 	address: '',
 	city: '',
 	state: '',
 	zipcode: '',
 	avatar: '',
-	getProfileUpdate: function(n, a, c, s, z, p) {
-		this.name = n;
-		this.address = a;
-		this.city = c;
-		this.state = s;
-		this.zipcode = z;
-		this.avatar = p;
-	}
 }
+
+
+
+// Write a function getProfileUpdate() that hypothetically asks the user to
+// update some profile properties and returns information about which properties
+// were changed, and to what values.(for now, just hard code the return value)
+
+getProfileUpdate: function(n, a, c, s, z, p) {
+    userProfile.prototype.name = n;
+    userProfile.prototype.address = a;
+    userProfile.prototype.city = c;
+    userProfile.prototype.state = s;
+    userProfile.prototype.zipcode = z;
+    userProfile.prototype.avatar = p;
+}
+
+
+// Write a function updateProfile() that takes a single object of keys:values
+// and overwrites those keys on the profile object
